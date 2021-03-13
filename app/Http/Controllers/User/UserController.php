dc17fa24f724eb13767a089e2509ab2f1a4e9e5d
vagrant <vagrant@homestead>
2021-03-13 19:36:02 +0000

    en esta parte en el metodo store() hemos recibido todos datos alrededor de peticion http despues hemos aplicado la regla para campos obligatorios y validacion etc ... luego hemos asignado los campos que debemos asignar por defecto asi se ingnora los valores recibidos por un cliente sea angular o react o cualquier cliente lo tratamos , luego hemos creado la instancia de creat , y finalmente hemos devuelto una respuesta en caso que se ha exitado la insersacion con codigo de 201 usando el metodo de laravel response()->json->() que nos permitira especificar la respuesta , Ojo cualquier fracaso en la insersacion laravel dispara una excepcion en desarollos posteriores veremos como manipular estos excepcion para mandar mensajes relacionados al typo de fallo al cliente

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -42,9 +42,50 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+     /* =>metodo store nos permite crear instancias de usuarios */
+     /* estamos obteniendo por defecto un $request ,basicamente por medio de este objeto request que ha sido inyectado por medio resolucion de dependencias vamos a obtener toda la informacion relacionada
+        con la peticion (http) lo cual incluye por supuesto los campos recibidos en la peticion para crear el usurio es decir nombre que se va  utulizar , correo electronico , contraseña eetcc    */
+    public function store(Request $request)  /* => para crear instancia la peticion se realiza por medio meto http post */
     {
-        //
+        $campos = $request->all(); /* $campos recibe todo que venga con la peticion   */ /* de este metodo tendremos en un array() ... lo que acabamos de mencionar es decir la infs que nos va traer objeto request */
+                                    /* sabemos tenemos que hacer un control del typado de datos que recibimos como si es email correcto o no , tambien no podemos hacer de uso todo los datos recibidos por los usuarios , ya sabemos los maliosos usuarios
+                                     atraves de herramientas del desarollador cuando se trata de aplicacion web o en este caso en especifico tratamos con Apis de frontend como Angular donde no podemos hacer uso de todos los datos que mandan sus formularios
+                                     por ejemplo lo que corresponde al campo[ admin y verified ] tienes que ser esteblecidas explicitamente por nosotros y no acorde al que el el usuario envia puesto que nosotros quien decidamos si el usuario es administrador
+                                     y si esta verificado o no */
+
+
+        /* => adicionalmente tenemos que validar que recibamos los campos minimos necesarios , validar que el email sera email valido , validar contraseña y que sea confirmada es decir se envia un campo de confirmacion para validar que el usuario esta
+          haciendo la contraseña que cree ,  */
+        /* REGLA DE VALIDACION  */
+        $rules = [
+          'name' => 'required',
+          'email' => 'required|email|unique:users',  /* tiene que unico en tabla users */
+          'password' => 'required|min:6|confirmed'  /* => tiene que ser confirmada es decir tenemos que recibir un campo llamado password_confirmation = password , mismo valor que password  = password  */
+        ];
+        /* ejecutar estas reglas validando la peticion , en caso no pasan laravel disparara una exception posteriormente vemos como controlar esta exception de manera sencilla  */
+        $this->validate($request, $rules); /* se hace atraves este metodo , pasando la peticion original y las reglas a usar  */
+
+
+
+        /* ASIGNACION POR DEFECTO  */  /* se toma estos valores y se ignora valor mandados por un cliente sea angular o react en estos campos en especifico , en casso password se toma valor incryptado porsupuesto  */
+
+        $campos['password'] = bcrypt($request->password); /* => el password tiene que increptarse de manera accorde a los requiremientos de db apartir del valor original por supuesto , usando el helpers bycripts  */
+        $campos['verified'] = User::USUARIO_NO_VERIFICADO;/* =>establecer el valor por defecto , Recuerda que los attributos que establecemos por defecto no debemos tomar en cuenta lo que el cliente frontend(angular) asigna como valor a este campo o atributo */
+        $campos['verification_token'] = User::generarVerificationToken();/* => token de verificacion , paraque un user verifique su correo electronico , asignamos por defecto ... */
+        $campos['admin'] = User::USUARIO_REGULAR; /* => asignamos por defecto que un usuario es un usario regular no es un administrador  */
+
+
+
+        /* => Ahora bien creamos una instancia por medio function create de orm  */
+        $usuario = User::create($campos);  /* la function create() hace lo que se conece como asignacion masiva es decir que estamos asignado inmediatamente todos attributos que correspondan a este instancia user , No estamos asignando uno por uno  */
+                                           /* estamos mandando todo por una vez por medio de un array en este caso el array es $campos , por la asignacion masiva , hemos aplicado lo que conoce como $fillable en el modelo para evitar inyecciones
+                                           es decir insersaciones que no queremos */
+
+
+
+        return response()->json(['data' => $usuario], 201); /* retornamos respuesta 201 indicando que ya se realizo la operacion de almacenamiento con exito */ /* si te fijas la contraseña y token ..etc no ha sido devuelta en respuesta debido a la propiedad de
+                                                              $hiden que usamos en nuestro modelo que es capaz de esconder los atributos que declaramos en su array. refiero a los atrributos del modelo user   */
+
     }
 
     /**
